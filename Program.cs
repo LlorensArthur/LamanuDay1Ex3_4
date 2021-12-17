dff1917289ad2a90abcd7e7b10d1a2a6b7418cf9
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-17 11:34:18 +0100

    Le numbre aléatoire est entre 1 et 50 comme demandé dnas l'exercice. Amélioré les commentaires.

--- Program.cs
+++ Program.cs
@@ -16,21 +16,22 @@ while(true)
         Console.WriteLine("Dommage, tu n'as plus d'essais");
         break;
     }
-    // Teste de la saisie utilisateur
+    // Vérification de la saisie. Tant que l'utilisateur ne rentre pas un nombre, affiche un message demandant un nombre.
     int answer = 0;
     while (!int.TryParse(Console.ReadLine(), out answer))
     {
         Console.WriteLine("Veuillez entrer uniquement un nombre.");
     }
+    // Vérifie si le nombre utilisateur est supérieur puis inférieur au nombre choisi par le programme
     if (answer > randomNumber)
     {
         Console.WriteLine("C’est plus petit");
-    }
+      }
     else if (answer < randomNumber)
     {
         Console.WriteLine("C’est plus grand");
     }
-    // Victoire
+    // Condition de victoire : le nombre utlilisateur est égal au nombre choisi par le programmer
     else if (answer == randomNumber)
     {
         Console.WriteLine($"Bravo vous avez trouvé en {currentTry} essais.");
